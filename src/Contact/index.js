05901db339c2c3f3f052e7839cd13d94b7d197e8
Misha Holzman <mcholzman1@gmail.com>
2018-11-20 19:18:45 -0500

    made About section better, created contact component, styling

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -4,12 +4,80 @@ import "./style.css";
 class ContactPage extends Component {
   render() {
     return (
-      <div className="about-page-container" id="contact-page">
-        <div className="">
-          <h2 className="about-title">contact me</h2>
+      <div className="conact-page-container" id="contact-page">
+        <img
+          src="https://www.freeiconspng.com/uploads/line-png-32.png"
+          width="40%"
+          alt="Free Download Line Png Images"
+          className="line"
+        />
+        <h2 className="contact-title">contact me</h2>
+        <div className="social-links">
+          <a
+            href="https://www.linkedin.com/in/misha-holzman/"
+            className="social-icon"
+            target="_blank"
+          >
+            <img
+              src="https://i.imgur.com/M427KnU.png"
+              className="social-images"
+              alt="linkedin"
+            />
+          </a>
+          <a
+            href="https://git.generalassemb.ly/mholzman"
+            className="social-icon"
+            target="_blank"
+          >
+            <img
+              src="https://i.imgur.com/0pzhAxs.png"
+              alt="github"
+              className="social-images"
+              height="50px"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/notions_of_real/?hl=en"
+            className="social-icon"
+            target="_blank"
+          >
+            <img
+              src="https://i.imgur.com/l9qpIO3.png"
+              className="social-images"
+              alt="instagram"
+            />
+          </a>
         </div>
-        <div className="">
-
+        <div className="contact-content-container">
+          <div className="">
+            <p className="phone">
+              <img
+                src="https://i.imgur.com/YBe6mxx.png"
+                alt="github"
+                height="30px"
+                className=""
+              />{" "}
+              347 • 603 • 6396
+            </p>
+            <p className="email">
+              <img
+                src="https://i.imgur.com/AGftEKD.png"
+                alt="github"
+                height="30px"
+                className=""
+              />{" "}
+              mcholzman1@gmail.com
+            </p>
+            <p className="address">
+              <img
+                src="https://i.imgur.com/C3FkxHo.png"
+                alt="github"
+                height="30px"
+                className=""
+              />
+              121 Wooster St. New York, NY 10012
+            </p>
+          </div>
         </div>
       </div>
     );
--- src/MyWork/index.js
+++ src/MyWork/index.js
@@ -4,50 +4,42 @@ import "./style.css";
 class MyWork extends Component {
   render() {
     return (
-      <div className="main-page-container">
+      <div className="main-work-container">
         <div id="press">
           <a id="my-work">
-            <h2 className="my-work-title">my work</h2>
+            <h2 className="my-work-title">MY WORK</h2>
           </a>
           <div className="press-container hover11">
-            <div>
+            <div className="shaka-div">
               <a
                 href="https://www.villagevoice.com/2016/09/28/a-bold-take-on-taiwanese-street-food-in-east-williamsburg/"
                 target="_blank"
               >
-                <img src="https://i.imgur.com/oUVUH32.png" alt="" />
+                <img src="https://i.imgur.com/bkN5v6m.jpg" alt="" className="shaka-site" height="300px" />
               </a>
             </div>
-            <div>
+            <div className="winson-div">
               <a
                 href="http://www.grubstreet.com/2016/05/win-son-modern-taiwanese-brooklyn-opening.html"
                 target="_blank"
               >
-                <img src="https://i.imgur.com/zWbKjhf.png" alt="" />
+                <img src="https://i.imgur.com/Zw3EHBL.jpg" alt="" height="320px" className="winson-site"/>
               </a>
             </div>
-            <div>
+            <div className="fooder-div">
               <a
                 href="https://www.villagevoice.com/2017/06/27/boys-meet-grill-when-the-guys-behind-win-son-got-together-it-was-love-at-first-bite/"
                 target="_blank"
               >
-                <img src="https://i.imgur.com/i2veK1o.png" alt="" />
+                <img src="https://i.imgur.com/Y8HWoHe.jpg" alt="" height="420px" className="fooder-site"/>
               </a>
             </div>
-            <div>
+            <div className="keeper-div">
               <a
                 href="https://ny.eater.com/2018/2/27/17053136/win-son-nyc-review-two-stars-taiwanese-american"
                 target="_blank"
               >
-                <img src="https://i.imgur.com/s1j1X8B.png" alt="" />
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://ny.eater.com/maps/best-taiwanese-food-nyc-brooklyn-queens-manhattan"
-                target="_blank"
-              >
-                <img src="https://i.imgur.com/BlwUBqT.png" alt="" />
+                <img src="https://i.imgur.com/hcaxG8m.jpg" alt="" className="keeper-site"/>
               </a>
             </div>
 
