618121ec3be4cdbfb7861f6da632f52ecf848878
Misha Holzman <mishaholzman@mishas-mbp.home>
2021-11-15 16:06:14 -0500

    fixed graphic design image

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './style.css'
-import { Link, animateScroll as scroll } from "react-scroll";
+// import { Link, animateScroll as scroll } from "react-scroll";
 
 import colorArrow from '../assets/arrow-color.png'
 
